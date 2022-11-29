dad4552c43b05687be89fd824105022adde0277d
cugbo <chimaugbo@gmail.com>
2022-11-29 13:31:22 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+print(", ".join("{0:0>2}".format(i) for i in range(100)))
