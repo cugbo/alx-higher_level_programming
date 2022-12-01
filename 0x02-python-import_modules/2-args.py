ba83cd348d39d65e5228d457305d46b2b078a36d
cugbo <chimaugbo@gmail.com>
2022-12-01 11:54:16 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
             print("{:d} argument:".format(total - 1))
         else:
             print("{:d} arguments:".format(total - 1))
-            for i in range(1, total):
-                print("{:d}: {}".format(i, sys.argv[i]))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))
