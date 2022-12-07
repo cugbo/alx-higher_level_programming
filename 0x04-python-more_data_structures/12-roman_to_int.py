5ed11ffedd888f309d02e91982f10879459fe69a
cugbo <chimaugbo@gmail.com>
2022-12-07 12:11:58 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    """ converts a Roman numeral to an integer."""
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
 
-def roman_to_int(r_str):
-    if not isinstance(r_str, str) or r_str is None:
-        return None
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number - 1, 0, -1):
+        current_value = romans[roman_string[i]]
+        previous_value = romans[roman_string[i-1]]
 
-    rom_var = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    ret = 0
-    for i in range(len(r_str)):
-        if r_str[i] not in rom_var:
-            return None
-        if i == len(r_str) - 1:
-            ret += rom_var[r_str[i]]
-        elif rom_var[r_str[i]] < rom_var[r_str[i + 1]]:
-            ret -= rom_var[r_str[i]]
+        if previous_value >= current_value:
+            value_int += previous_value
         else:
-            ret += rom_var[r_str[i]]
-    return ret
+            value_int -= previous_value
+
+    return value_int
